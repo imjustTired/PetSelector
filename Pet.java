41364ab5f9e83f84837a5ede4fc713ffc666346e
imjustTired <rodwelljoshalfred@gmail.com>
2025-10-16 18:28:54 +0000

    l;nh

--- Pet.java
+++ Pet.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public class Pet{
 
+    public static void main (String []args) {
+        //ask the user for input( name, season, and color)
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+    }
+}
+
+
+
+
+
+
+//make sure the input is valid
+// make a decision
+// tell the user what they get
