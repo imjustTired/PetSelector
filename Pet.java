9119c4724fed4db93cf2b1f745e4b202ce84a5dd
imjustTired <rodwelljoshalfred@gmail.com>
2025-10-23 18:28:06 +0000

    gg

--- Pet.java
+++ Pet.java
@@ -1,11 +1,18 @@
+import java.awt.Color;
 import java.util.*;
 public class Pet{
 
     public static void main (String []args) {
         //ask the user for input( name, season, and color)
         Scanner input = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = input.nextLine();
+        System.out.println("What is your favorite color");
+        String color = input.nextLine().toLowerCase().trim();
+        if (!(color.equals("red") || color.equals("blue") || color.equals("green")))
+        {
+            System.out.print("Please pick red, blue, or green");
+        }
+
+
     }
 }
 
@@ -15,5 +22,6 @@ public class Pet{
 
 
 //make sure the input is valid
+
 // make a decision
 // tell the user what they get
